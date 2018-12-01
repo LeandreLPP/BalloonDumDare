32cdb278ee1aab2314a57d9df4123ca1b56b9448
Léandre <leandre.lepolles@free.fr>
2018-12-01 17:07:41 +0100

    Improved movement by balloon

--- Prefabs/Balloon.gd
+++ Prefabs/Balloon.gd
@@ -2,27 +2,19 @@ extends RigidBody2D
 
 var inflated = false
 var deflating = false
+export (int) var propulsion_force = 500
 
-signal finish_inflating;
-signal finish_deflating;
+signal finish_inflating
+signal finish_deflating
 
 func _ready():
 	gravity_scale = 0
-	mode = RigidBody2D.MODE_CHARACTER
+	mode = RigidBody2D.MODE_STATIC
 
 func inflate(siding_left):
 	if not inflated:
 		$Animation.play("inflate")
 		$inflating_sound.play()
-
-func pierce(direction):
-	if inflated:
-		look_at(position + direction)
-		mode = RigidBody2D.MODE_CHARACTER
-		apply_impulse(Vector2(0,0), direction)
-		deflating = true
-		$deflating_sound.play()
-		$DeflatingTimer.start()
 
 func _on_animation_finished(var animationName):
 	if not inflated:
@@ -30,6 +22,17 @@ func _on_animation_finished(var animationName):
 		gravity_scale = -1
 		mode = RigidBody2D.MODE_RIGID
 		emit_signal("finish_inflating")
+		
+func pierce(direction):
+	if inflated:
+		mass = 10
+		$Head.add_force(Vector2(0,0), direction * mass * propulsion_force)
+		deflating = true
+		$deflating_sound.play()
+		$DeflatingTimer.start()
 
 func _on_DeflatingTimer_timeout():
+	emit_signal("finish_deflating")
+
+func _on_deflating_sound_finished():
 	emit_signal("finish_deflating")
