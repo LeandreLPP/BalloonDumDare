2022b81c60e3995ea4e1020b87aff9c81669e04a
Léandre <leandre.lepolles@free.fr>
2018-12-01 14:51:53 +0100

    Disabled Balloon gravity until finished inflating

--- Prefabs/Balloon.gd
+++ Prefabs/Balloon.gd
@@ -6,23 +6,30 @@ var deflating = false
 signal finish_inflating;
 signal finish_deflating;
 
-func inflate():
+func _ready():
+	gravity_scale = 0
+	mode = RigidBody2D.MODE_CHARACTER
+
+func inflate(siding_left):
 	if not inflated:
-		$AnimatedSprite.play("inflating")
+		$Animation.play("inflate")
 		$inflating_sound.play()
 
-func pierce(var direction):
+func pierce(direction):
 	if inflated:
+		look_at(position + direction)
+		mode = RigidBody2D.MODE_CHARACTER
 		apply_impulse(Vector2(0,0), direction)
 		deflating = true
 		$deflating_sound.play()
 		$DeflatingTimer.start()
 
-func _on_animation_finished():
+func _on_animation_finished(var animationName):
 	if not inflated:
 		inflated = true
+		gravity_scale = -1
+		mode = RigidBody2D.MODE_RIGID
 		emit_signal("finish_inflating")
-		$AnimatedSprite.play("inflated")
 
 func _on_DeflatingTimer_timeout():
 	emit_signal("finish_deflating")
