5da59f3073e79f0c8f81cbee57e7d55e92d90e07
Léandre <leandre.lepolles@free.fr>
2018-12-01 11:39:47 +0100

    Added movement for player

--- Prefabs/Player.gd
+++ Prefabs/Player.gd
@@ -1,23 +1,140 @@
 extends RigidBody2D
 
-export (int) var speed  # How fast the player will move (pixels/sec).
-var screensize  # Size of the game window.
+# Member variables
+var anim = ""
+var siding_left = false
+var jumping = false
+var stopping_jump = false
 
-func _ready():
-	screensize = get_viewport_rect().size
+export (int) var WALK_ACCEL = 800.0
+export (int) var WALK_DEACCEL = 800.0
+export (int) var WALK_MAX_VELOCITY = 200.0
+export (int) var AIR_ACCEL = 200.0
+export (int) var AIR_DEACCEL = 200.0
+export (int) var JUMP_VELOCITY = 460
+export (int) var STOP_JUMP_FORCE = 900.0
 
-func _process(delta):
-	var velocity = Vector2() # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
-		self.linear_velocity = velocity * delta
-		position.x = clamp(position.x, 0, screensize.x)
-		position.y = clamp(position.y, 0, screensize.y)
+var MAX_FLOOR_AIRBORNE_TIME = 0.15
+
+var airborne_time = 1e20
+
+var floor_h_velocity = 0.0
+
+func _integrate_forces(var s):
+	var lv = s.get_linear_velocity()
+	var step = s.get_step()
+	
+	var new_anim = anim
+	var new_siding_left = siding_left
+	
+	# Get the controls
+	var move_left = Input.is_action_pressed("move_left")
+	var move_right = Input.is_action_pressed("move_right")
+	var jump = Input.is_action_pressed("jump")
+	
+	if jump:
+		jump = true
+	
+	# Deapply prev floor velocity
+	lv.x -= floor_h_velocity
+	floor_h_velocity = 0.0
+	
+	# Find the floor (a contact with upwards facing collision normal)
+	var found_floor = false
+	var floor_index = -1
+	
+	for x in range(s.get_contact_count()):
+		var ci = s.get_contact_local_normal(x)
+		if ci.dot(Vector2(0, -1)) > 0.6:
+			found_floor = true
+			floor_index = x
+	
+	if found_floor:
+		airborne_time = 0.0
 	else:
-		$AnimatedSprite.stop()
-	$AnimatedSprite.flip_h = velocity.x < 0
+		airborne_time += step # Time it spent in the air
+	
+	var on_floor = airborne_time < MAX_FLOOR_AIRBORNE_TIME
+
+	# Process jump
+	if jumping:
+		if lv.y > 0:
+			# Set off the jumping flag if going down
+			jumping = false
+		elif not jump:
+			stopping_jump = true
+		
+		if stopping_jump:
+			lv.y += STOP_JUMP_FORCE * step
+	
+	if on_floor:
+		# Process logic when character is on floor
+		if move_left and not move_right:
+			if lv.x > -WALK_MAX_VELOCITY:
+				lv.x -= WALK_ACCEL * step
+		elif move_right and not move_left:
+			if lv.x < WALK_MAX_VELOCITY:
+				lv.x += WALK_ACCEL * step
+		else:
+			var xv = abs(lv.x)
+			xv -= WALK_DEACCEL * step
+			if xv < 0:
+				xv = 0
+			lv.x = sign(lv.x) * xv
+		
+		# Check jump
+		if not jumping and jump:
+			lv.y = -JUMP_VELOCITY
+			jumping = true
+			stopping_jump = false
+			$sound_jump.play()
+		
+		# Check siding
+		if lv.x < 0 and move_left:
+			new_siding_left = true
+		elif lv.x > 0 and move_right:
+			new_siding_left = false
+		if jumping:
+			new_anim = "jumping"
+		elif abs(lv.x) < 0.1:
+			new_anim = "idle"
+		else:
+			new_anim = "run"
+	else:
+		# Process logic when the character is in the air
+		if move_left and not move_right:
+			if lv.x > -WALK_MAX_VELOCITY:
+				lv.x -= AIR_ACCEL * step
+		elif move_right and not move_left:
+			if lv.x < WALK_MAX_VELOCITY:
+				lv.x += AIR_ACCEL * step
+		else:
+			var xv = abs(lv.x)
+			xv -= AIR_DEACCEL * step
+			if xv < 0:
+				xv = 0
+			lv.x = sign(lv.x) * xv
+		
+		if lv.y < 0:
+			new_anim = "jumping"
+		else:
+			new_anim = "falling"
+	
+	# Update siding
+	if new_siding_left != siding_left:
+		$anim.flip_h = new_siding_left
+		siding_left = new_siding_left
+	
+	# Change animation
+	if new_anim != anim:
+		anim = new_anim
+		$anim.play(anim)
+	
+	# Apply floor velocity
+	if found_floor:
+		floor_h_velocity = s.get_contact_collider_velocity_at_position(floor_index).x
+		lv.x += floor_h_velocity
+	
+	# Finally, apply gravity and set back the linear velocity
+	lv += s.get_total_gravity() * step
+	s.set_linear_velocity(lv)
