1213040d6b48f35b8cf0833cca3a62e69151c1a0
Léandre <leandre.lepolles@free.fr>
2018-12-02 23:20:27 +0100

    Fixed Credit Scene

--- Scenes/CreditsScene.gd
+++ Scenes/CreditsScene.gd
@@ -9,11 +9,12 @@ func _on_MenuButton_pressed():
 	LevelManager.goto_main_menu()
 
 func _on_TimerPopBalloon_timeout():
+	var spawn = $MarginContainer/VBoxContainer/PathPop/BalloonSpawnLocation
 	# Choose a random location on Path2D.
-	$PathPop/BalloonSpawnLocation.set_offset(randi())
+	spawn.set_offset(randi())
 	# Create a Mob instance and add it to the scene.
 	var balloon = Balloon.instance()
 	add_child(balloon)
 	balloon.get_child(3).stream = null
-	balloon.position = $PathPop/BalloonSpawnLocation.position
+	balloon.position = spawn.position
 	balloon.inflate(false)
