6181fa5f9f6409b1a0e6758360704e6ed651cd8c
Léandre <leandre.lepolles@free.fr>
2018-12-02 13:57:07 +0100

    Sprite and sound of balloon switch

--- Prefabs/BalloonSwitch.gd
+++ Prefabs/BalloonSwitch.gd
@@ -2,15 +2,13 @@ extends Area2D
 
 export (bool) var RESETABLE = false
 
-signal switched(on)
-
-var on = false
+signal pressed
 
 func _on_BalloonSwitch_body_entered(body):
 	print("body entered" + self.get_path_to(body))
 	var p = self.get_path_to(body)
 	p = p.get_name(p.get_name_count() - 1)
 	if p == "Balloon" or p.begins_with("@Balloon@"):
-		if not on or RESETABLE :
-			on = !on
-			emit_signal("switched", on)
+		emit_signal("pressed")
+		$AnimatedSprite.play("on")
+		$Sound.play()
