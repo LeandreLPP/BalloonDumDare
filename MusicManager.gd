ef60359352cfdb96dd6f1ca8a84880f392648db6
Tom Spreters <tom.spreters@gmail.com>
2018-12-02 22:44:52 +0100

    Make main music stoppables at more points

--- MusicManager.gd
+++ MusicManager.gd
@@ -2,14 +2,30 @@ extends Node
 
 export (bool) var inGame = false
 
+var mainCurrent = 0
+
 func _on_MenuPlayer_finished():
 	if not inGame:
 		$MenuPlayer.play()
 	else:
-		$MainPlayer.play()
+		playNextMain()
 
 func _on_MainPlayer_finished():
 	if not inGame:
 		$MenuPlayer.play()
 	else:
-		$MainPlayer.play()
+		playNextMain()
+
+func playNextMain():
+	mainCurrent += 1
+	if mainCurrent > 4:
+		mainCurrent = 1
+	
+	if mainCurrent == 1:
+		$Main1Player.play()
+	if mainCurrent == 2:
+		$Main2Player.play()
+	if mainCurrent == 3:
+		$Main3Player.play()
+	if mainCurrent == 4:
+		$Main4Player.play()
