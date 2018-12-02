6cb14f76d68537d9264418eb1c574748298dde34
Léandre <leandre.lepolles@free.fr>
2018-12-02 23:58:39 +0100

    Improved Main and Options menus

--- Prefabs/Rope.gd
+++ Prefabs/Rope.gd
@@ -1,10 +1,13 @@
 extends Line2D
 
+export (NodePath) var surchage_hand
 var hand
 
 func _process(delta):
 	rotation = -$"..".rotation
-	if hand:
+	if hand or surchage_hand:
+		if not hand:
+			hand = get_node(surchage_hand)
 		var pos_point = hand.get_global_position() - get_global_position()
 		if get_point_count() < 2:
 			add_point(pos_point)
