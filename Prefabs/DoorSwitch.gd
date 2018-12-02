3203aff01853294a882ef1c1656794ef19cfc20c
Léandre <leandre.lepolles@free.fr>
2018-12-02 22:02:34 +0100

    Level 9

--- Prefabs/DoorSwitch.gd
+++ Prefabs/DoorSwitch.gd
@@ -1,5 +1,9 @@
 extends StaticBody2D
 
 func open():
-	$AnimatedSprite.play("open")
+	$AnimatedSprite.play("opened")
 	$CollisionShape2D.set_disabled(true)
+
+func close():
+	$AnimatedSprite.play("closed")
+	$CollisionShape2D.set_disabled(false)
